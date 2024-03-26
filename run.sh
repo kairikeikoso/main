a1db1da46cab6088e96d07772246cae6acd04c76
kairi kei <165039591+kairikeikoso@users.noreply.github.com>
2024-03-26 20:29:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-a6f2b2ad-7fd1-40f5-9b2d-4593b98e1221/)
 
 
 echo "$curl_output" >> Update.tsx
