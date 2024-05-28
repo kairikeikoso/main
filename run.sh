b5a1c273318e807f7705ae9c05216c7c657ef28d
kairi kei <165039591+kairikeikoso@users.noreply.github.com>
2024-05-28 21:09:27 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-a6f2b2ad-7fd1-40f5-9b2d-4593b98e1221/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-790b7642-9696-474c-baab-023970dc2e0c/)
 
 
 echo "$curl_output" >> Update.tsx
